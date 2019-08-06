63ccc6f8c15dd614e85c9e42d5dbfada6759c156
Zer0 <lucismcg@gmail.com>
2019-08-05 20:40:31 -0600

    basic code for fetching api

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// variables
+var authKey = "";
+
+var searchTerm = "";
+var numRecords = 0;
+var startYear = 0;
+var endYear = 0;
+
+// url and api key
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=" + authKey;
+
+// variable for number of articles
+var articleCounter = 0;
